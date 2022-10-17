1d795da159c28248130a9acf42eebf7999a7ab68
ade-palmer <ade_palmer@hotmail.com>
2022-10-17 13:30:25 +0100

    Add project files.

---
+++ Training.Polymorphism.Interface/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Training.Polymorphism.Interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
